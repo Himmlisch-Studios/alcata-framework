b96f3043c217a8db2f1b816b708d5abf95aba711
Luan Himmlisch <luan@himmlisch.com.mx>
2023-09-30 23:30:36 -0600

    Fixed Vite not loading .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { PluginOption, defineConfig } from 'vite';
+import { PluginOption, defineConfig, loadEnv } from 'vite';
 import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
 
 const isProduction = process.env.NODE_ENV == 'production';
@@ -9,33 +9,37 @@ const fullReloadAlways: PluginOption = {
     }
 } as PluginOption
 
-export default defineConfig({
-    root: './src',
-    build: {
-        outDir: '../dist',
-        minify: false,
-        emptyOutDir: true,
-    },
-    json: {
-        stringify: true
-    },
-    plugins: [
-        ...(isProduction ? [
-            obfuscatorPlugin({
-                include: ['src/js/**/*.js'],
-                exclude: [/node_modules/],
-                options: {
-                    controlFlowFlattening: true,
-                    deadCodeInjection: true,
-                    debugProtection: true,
-                    disableConsoleOutput: true,
-                    numbersToExpressions: true,
-                    // renameProperties: true, //! May break Alpine
-                    // selfDefending: true //! May break Alpine
-                }
-            })
-        ] : [
-            fullReloadAlways
-        ]),
-    ],
-});
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        root: './src',
+        build: {
+            outDir: '../dist',
+            minify: false,
+            emptyOutDir: true,
+        },
+        json: {
+            stringify: true
+        },
+        plugins: [
+            ...(isProduction ? [
+                obfuscatorPlugin({
+                    include: ['src/js/**/*.js'],
+                    exclude: [/node_modules/],
+                    options: {
+                        controlFlowFlattening: true,
+                        deadCodeInjection: true,
+                        debugProtection: true,
+                        disableConsoleOutput: true,
+                        numbersToExpressions: true,
+                        // renameProperties: true, //! May break Alpine
+                        // selfDefending: true //! May break Alpine
+                    }
+                })
+            ] : [
+                fullReloadAlways
+            ]),
+        ],
+    })
+};
