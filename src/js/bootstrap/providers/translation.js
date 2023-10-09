73d165c97a7e8f4952376ac41a2c2490c6e208ea
Luan Himmlisch <luan@himmlisch.com.mx>
2023-10-08 22:39:57 -0600

    Removed unused variable

--- src/js/bootstrap/providers/translation.js
+++ src/js/bootstrap/providers/translation.js
@@ -8,8 +8,6 @@ export const TranslationProvider = {
     locale: 'en',
     fallbackLocale: 'en',
     boot() {
-        const app = window.Alpine.store('app');
-
         window.AlpineI18n.fallbackLocale = this.fallbackLocale;
         window.AlpineI18n.create(this.locale, locale);
     }
